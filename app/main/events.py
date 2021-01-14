9f294f031e2b4fdcf9c08b2754f88ed43b4af355
KhalidObaide <sayedkhalidobaide@gmail.com>
2021-01-14 15:22:30 +0430

    Styled and added the emoji picker to the chat page

--- app/main/events.py
+++ app/main/events.py
@@ -17,14 +17,15 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {'msg': session.get('name') + '![SEPERATE_NAME_FROM_THE_MESSAGE_CODE]!' + message['msg'],'id' : message['id']}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
 def left(message):
     """Sent by clients when they leave a room.
     A status message is broadcast to all people in the room."""
+    print("LEAVED")
     room = session.get('room')
     leave_room(room)
-    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+    emit('status', {'msg': ' ![SEPERATE_NAME_FROM_THE_MESSAGE_CODE]!' + session.get('name') + ' has left the room.'}, room=room)
 
--- chat.py
+++ chat.py
@@ -4,4 +4,4 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0")
