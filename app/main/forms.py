651c7a0b462e61f54454fb3e4cb0a1aaffa76ff0
Mike Davlantes <rocks722@hotmail.com>
2022-04-09 10:24:10 -0500

    Upgrade packages to the latest versions (#36)

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     """Accepts a nickname and a room."""
-    name = StringField('Name', validators=[Required()])
-    room = StringField('Room', validators=[Required()])
+    name = StringField('Name', validators=[DataRequired()])
+    room = StringField('Room', validators=[DataRequired()])
     submit = SubmitField('Enter Chatroom')
