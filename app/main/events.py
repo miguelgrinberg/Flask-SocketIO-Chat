43c0b87399a2dbd86a74907e07be418bbdff85f0
parth-lth <parthpkul@gmail.com>
2024-06-18 02:11:50 +0530

    Add message date and time

--- app/main/events.py
+++ app/main/events.py
@@ -1,6 +1,7 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
+import datetime
 
 
 @socketio.on('joined', namespace='/chat')
@@ -17,7 +18,9 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("Current Time: ", current_time)
+    emit('message', {'msg': '[' + current_time + ']' + session.get('name') + ':' + message['msg']}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
