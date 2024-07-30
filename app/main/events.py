0c5ca7d3b66f0e418d14b01618b4389a5a7b019b
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-07-31 00:10:44 +0100

    show timestamp in all messages

--- app/main/events.py
+++ app/main/events.py
@@ -1,3 +1,4 @@
+from time import time
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
@@ -9,7 +10,9 @@ def joined(message):
     A status message is broadcast to all people in the room."""
     room = session.get('room')
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    emit('status', {
+        'msg': f"{session.get('name')} has entered the room.",
+    }, to=room)
 
 
 @socketio.on('text', namespace='/chat')
@@ -17,7 +20,9 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {
+        'msg': f"{session.get('name')}: {message['msg']}",
+    }, to=room)
 
 
 @socketio.on('left', namespace='/chat')
@@ -26,5 +31,7 @@ def left(message):
     A status message is broadcast to all people in the room."""
     room = session.get('room')
     leave_room(room)
-    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+    emit('status', {
+        'msg': f"{session.get('name')} has left the room.",
+    }, to=room)
 
